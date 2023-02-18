57c4d5e655365a0e22937fa85a5c5cbc4d315bfd
Nguyen Trong Quyet <super.quyet69@gmail.com>
2023-02-18 12:15:55 +0700

    end nodejs

--- src/controller/homecontroller.js
+++ src/controller/homecontroller.js
@@ -1,4 +1,5 @@
 import connectionpromise from '../configs/connectDB'
+const multer  = require('multer')
 
 let gethomepage = async(req,res)=>{
     const [rows,fields] = await connectionpromise.query( 'SELECT * FROM `users`');
@@ -36,6 +37,57 @@ let updateuser =async(req,res)=>{
 let uploadfile =(req,res) => {
     return res.render('upload.ejs');
 }
+let profile = async(req,res) => {
+    upload(req, res, function(err) {
+        // req.file contains information of uploaded file
+        // req.body contains information of text fields, if there were any
+
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        else if (!req.file) {
+            return res.send('Please select an image to upload');
+        }
+        else if (err instanceof multer.MulterError) {
+            return res.send(err);
+        }
+        else if (err) {
+            return res.send(err);
+        }
+
+        // Display uploaded image for user validation
+        res.send(`You have uploaded this image: <hr/><img src="/images/${req.file.filename}" width="500"><hr /><a href="./">Upload another image</a>`);
+    });
+}
+const upload =multer().single('profile_pic')
+const uploadmutil = multer().array('multiple_images' );
+let uploadmultiple =async(req, res) => {
+    uploadmutil(req, res, function(err) {
+        if (req.fileValidationError) {
+            return res.send(req.fileValidationError);
+        }
+        else if (!req.files) {
+            return res.send('Please select an image to upload');
+        }
+        else if (err instanceof multer.MulterError) {
+            return res.send(err);
+        }
+        else if (err) {
+            return res.send(err);
+        }
+
+        let result = "You have uploaded these images: <hr />";
+        const files = req.files;
+        let index, len;
+
+        // Loop through all the uploaded images and display them on frontend
+        for (index = 0, len = files.length; index < len; ++index) {
+            result += `<img src="/images/${files[index].filename}" width="300" style="margin-right: 20px;">`;
+        }
+        result += '<hr/><a href="./">Upload more images</a>';
+        res.send(result);
+    });
+}
 module.exports ={
     gethomepage,
     detailuser,
@@ -43,5 +95,7 @@ module.exports ={
     deleteuser,
     edituser,
     updateuser,
-    uploadfile
+    uploadfile,
+    profile,
+    uploadmultiple
 }
--- src/route/web.js
+++ src/route/web.js
@@ -1,7 +1,32 @@
 import express from "express";
 import homecontroller from "../controller/homecontroller"
-
+import  path  from "path";
+const multer  = require('multer')
+const appRoot = require('app-root-path');
 let router = express.Router();
+
+const storage = multer.diskStorage({
+    destination: function(req, file, cb) {
+        cb(null, appRoot+'/src/public/image');
+    },
+
+    // By default, multer removes file extensions so let's add them back
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+const imageFilter = function(req, file, cb) {
+    // Accept images only
+    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+        req.fileValidationError = 'Only image files are allowed!';
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+
+let upload = multer({ storage: storage, fileFilter: imageFilter });
+
+
 const initwebroute=(app)=>{
 router.get('/',homecontroller.gethomepage)
 router.get('/detail/:id', homecontroller.detailuser)
@@ -10,6 +35,9 @@ router.post('/updateuser',homecontroller.updateuser)
 router.get('/delete/:id',homecontroller.deleteuser)
 router.get('/edituser/:id',homecontroller.edituser)
 router.get('/upload',homecontroller.uploadfile)
+router.post('/upload-profile-pic', upload.single('profile_pic'), homecontroller.profile)
+router.post('/upload-multiple-images',upload.array('multiple_images',3),homecontroller.uploadmultiple)
+
 return app.use('/', router)
 
 }
